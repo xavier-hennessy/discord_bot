39004068f53cb4170fe4cf26c2a342e18fbee273
Xavier <xavier@bitreel.com>
2022-06-15 11:28:44 +1000

    swear i did this before

--- app/discord/index.js
+++ app/discord/index.js
@@ -13,12 +13,16 @@ export default class DiscordClient {
         this.BOT_TOKEN = BOT_TOKEN;
         this.PREFIX = PREFIX;
         this.client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+        this.dbClient = null;
+        this.alchemyClient = null;
         this.channel = null;
     }
 
     init(dbClient, alchemyClient) {
         this.connectToDiscord();
-        this.listenForMessages(dbClient, alchemyClient);
+        this.listenForMessages();
+        this.dbClient = dbClient;
+        this.alchemyClient = alchemyClient;
     }
 
     connectToDiscord = async () => {
@@ -31,7 +35,7 @@ export default class DiscordClient {
         }
     }
 
-    listenForMessages = (db, ws) => {
+    listenForMessages = () => {
         console.log("listening for messages...")
         this.client.on("messageCreate", async (message) => {
             this.channel = await this.client.channels.fetch(message.channelId);
@@ -53,18 +57,18 @@ export default class DiscordClient {
             switch (command) {
                 case FOLLOW_WALLET:
                     message.reply(`ok, following ${args[1] ? args[1] : args[0]}..`)
-                    const follow = await db.followWallet({ address: args[0], label: args[1] });
-                    ws.subscribeToFilteredTransactions(this.channel, follow);
+                    const follow = await this.dbClient.followWallet({ address: args[0], label: args[1] });
+                    this.alchemyClient.subscribeToFilteredTransactions(this.channel, follow);
                     console.log(follow);
                     break
                 case UNFOLLOW_WALLET:
                     message.reply(`ok, unfollowing ${args[1] ? args[1] : args[0]}..`);
-                    const unfollow = await db.unfollowWallet(args[0]);
+                    const unfollow = await this.dbClient.unfollowWallet(args[0]);
                     console.log(unfollow)
                     break
                 case REMOVE_WALLET:
-                    const remove = await db.removeWallet(args[0]);
-                    ws.unsubscribeFromAllTransactions();
+                    const remove = await this.dbClient.removeWallet(args[0]);
+                    this.alchemyClient.unsubscribeFromAllTransactions();
                     if (remove !== args[0]) {
                         message.reply(`ok, removed ${args[1] ? args[1] : args[0]} delete count: ${remove.deletedCount}`);
                     } else {
@@ -73,7 +77,7 @@ export default class DiscordClient {
                     console.log(remove)
                     break
                 case GETALLWALLETS:
-                    const wallets = await db.getAllWallets()
+                    const wallets = await this.dbClient.getAllWallets()
                     message.reply(`ok, found ${wallets.length} wallets..`);
                     wallets.forEach((w) => {
                         message.reply(`address: ${w.address}, label: ${w.label ? w.label : ''}, following: ${w.follow}`);
@@ -82,12 +86,12 @@ export default class DiscordClient {
                     break
                 case FOLLOW_ALL:
                     message.reply(`ok, following all transactions...`);
-                    ws.subscribeToAllTransactions(this.channel)
+                    this.alchemyClient.subscribeToAllTransactions(this.channel)
                     break
                 case REMOVE_ALL:
                     message.reply(`ok, following all transactions...`);
                     console.log("in here")
-                    ws.unsubscribeToAllTransactions(this.channel)
+                    this.alchemyClient.unsubscribeToAllTransactions(this.channel)
                     break
                 default:
                     break
